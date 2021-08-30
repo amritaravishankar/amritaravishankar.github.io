929a54821dafb41050f6dea495f079c865c1a63e
amritaravishankar <amritaravishankar00@gmail.com>
2021-08-30 18:14:37 +0800

    update skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,6 +199,23 @@
     }
   });
 
+    /**
+   * Skills animation
+   */
+     let skilsContent = select('.skills-content');
+     if (skilsContent) {
+       new Waypoint({
+         element: skilsContent,
+         offset: '80%',
+         handler: function(direction) {
+           let progress = select('.progress .progress-bar', true);
+           progress.forEach((el) => {
+             el.style.width = el.getAttribute('aria-valuenow') + '%'
+           });
+         }
+       })
+     }
+
   /**
    * Portfolio details slider
    */
